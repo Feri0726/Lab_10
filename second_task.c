a4721701c00aa22245be4df662196e70ac50c9fc
Feri0726 <feherferi2007@gmail.com>
2025-11-12 15:46:55 +0000

    task 2 done

--- second_task.c
+++ second_task.c
@@ -11,7 +11,18 @@ Use the debugger to trace the stack!
 
 #include <stdio.h>
 
+int fib(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return (fib(n-1)+fib(n-2));
+    
+
+}
+
 int main(){
+    for (int i = 0; i < 10; i++){
+        printf("%d\t", fib(i));
+    }
 
 return 0;
 }
