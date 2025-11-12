62e60b833bfb7e261dd225f847693117784d0407
Feri0726 <feherferi2007@gmail.com>
2025-11-12 16:47:22 +0000

    sixth done

--- sixth_task.c
+++ sixth_task.c
@@ -1,7 +1,7 @@
 /* Task description:
 Pretty print numbers
 
-Write a function that prints the positive integer received as a parameter in an easy to read format: form groups of three digits and separate them by spaces. 
+Write a function that prints the positive integer received as a parameter in an easy to read format: form groups of three digits and separate them by spaces.
 Example: 16 077 216. Test for other values, too: 999, 1000, 12, 0, 1000222.
 
 Hint:
@@ -10,11 +10,18 @@ To printf 3 digits with leading zeros use %03d in the format string.
 
 */
 
-
 #include <stdio.h>
 
-int main(){
+int pretty(int n)
+{
+  if (n >= 1000)
+    pretty(n / 1000);
+  printf("%d ", n % 1000);
+}
 
+int main()
+{
+  pretty(1121529);
 
   return 0;
 }
